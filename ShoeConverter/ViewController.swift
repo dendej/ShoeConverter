af7a9de4175dfcee9987f160defe1ea162150a41
dendej <dendej@gmail.com>
2014-11-25 14:37:36 +0700

    Adding some UIs and Functionalities

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menShoeSizeTextField: UITextField!
+    @IBOutlet weak var menConvertedShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+//        let sizeFromTextField = menShoeSizeTextField.text
+//        let numberFromTextField = sizeFromTextField.toInt()
+        let sizeFromTextField = menShoeSizeTextField.text.toInt()!
+        let conversionConstant = 30
+        menConvertedShoeSizeLabel.hidden = false
+        menConvertedShoeSizeLabel.text = "\(conversionConstant + sizeFromTextField)" + "in European Shoe Size"
+    }
 
 }
 
